db915d6824721351b25dd439e65be3b892cd78a6
tsw0015 <tsw0015@mix.wvu.edu>
2021-08-31 14:53:23 -0700

    Top NavBar Finished. File Structure for Bottom NavBar added.

---
+++ src/app/headers/bottom-nav-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    selector: 'app-bottom-nav-bar',
+    templateUrl: './bottom-nav-bar.component.html'
+})
+
+export class BottomNavBarComponent {
+
+}
