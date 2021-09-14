4b98d80704dfb4f0c088c3a5102e68d43d4443f6
Tyler Wolf <tylerwolf2290@gmail.com>
2021-09-14 19:38:10 -0400

    modifications to class attributes

--- src/app/top-carousel/top-carousel.component.ts
+++ src/app/top-carousel/top-carousel.component.ts
@@ -7,7 +7,14 @@ import { Component } from "@angular/core";
 })
 
 export class TopCarouselComponent {
-        head1:string = "Soul, Electrified."
-        sub1:string = "The new all-electric Taycan. From $82,700.*"
-        head2:string = "Sportscar Together Fest 2021."
+        head1 = "Soul, Electrified."
+        sub1 = "The new all-electric Taycan. From $82,700.*"
+        head2 = "Sportscar Together Fest 2021."
+        sub2 = "A celebration of car culture. Learn more."
+        head3 = "Choose Thrilling."
+        sub3 = "The Macan Turbo."
+        head4 = "Soul, electrified."
+        sub4 = "The new Taycan Cross Turismo."
+        head5 = "Visualize, Customize, Actualize."
+        sub5 = "Build your dream Taycan Cross Turismo today."
 }
