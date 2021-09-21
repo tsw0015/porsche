973aa909a1fd0747dbdb8c69bb7b76ffe823e3ca
tsw0015 <tsw0015@mix.wvu.edu>
2021-09-21 13:44:16 -0700

    Modified routing paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LayoutMainComponent } from './layout/main/layout-main.component';
 import { LayoutCompareComponent } from './layout/compare/layout-compare.component';
 import { LayoutBuildComponent } from './layout/build/layout-build.component';
 import { LayoutOfferComponent } from './layout/offer/layout-offer.component';
 import { LayoutFinancialComponent } from './layout/financial/layout-financial.component';
+import { LayoutHomeComponent } from './layout/home/layout-home.component';
 
 const routes: Routes = [
-  { path: 'home', component: LayoutMainComponent },
+  { path: 'home', component: LayoutHomeComponent },
   { path: 'compare', component: LayoutCompareComponent },
   { path: 'build', component: LayoutBuildComponent },
   { path: 'offer', component: LayoutOfferComponent },
