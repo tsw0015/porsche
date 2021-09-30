9251d9d564ee3cb564693fbab85ea517479150a7
tsw0015 <tsw0015@mix.wvu.edu>
2021-09-30 12:00:46 -0700

    changes to styling

--- src/app/car-card/mock-cars.ts
+++ src/app/car-card/mock-cars.ts
@@ -2,16 +2,16 @@ export const mock_cars = [
     {
         model: "718",
         price: "$60,500",
-        picture: "./assets/718Card.jpg"
+        picture: "/home/tyler/angular_ws/porsche/src/assets/718Card.jpg"
     },
     {
         model: "911",
         price: "$101,200",
-        picture: "./assets/911Card.jpg"
+        picture: "/home/tyler/angular_ws/porsche/src/assets/718Card.jpg"
     },
     {
         model: "Taycan",
         price: "$82,700",
-        picture: "./assets/taycanCard.jpg"
+        picture: "/home/tyler/angular_ws/porsche/src/assets/718Card.jpg"
     }
 ]
