bef49fa8dd05296243b5e6d6a30fb0973b7e4a83
tsw0015 <tsw0015@mix.wvu.edu>
2021-10-26 17:39:16 -0700

    Fixed merge stuff

--- src/app/car-card/mock-cars.ts
+++ src/app/car-card/mock-cars.ts
@@ -1,17 +1,17 @@
 export const mock_cars = [
-        {
-            model: "718",
-            price: "$60,500",
-            picture: "/home/tyler/angular_ws/porsche/src/assets/718Card.jpg"
-        },
-        {
-            model: "911",
-            price: "$101,200",
-            picture: "/home/tyler/angular_ws/porsche/src/assets/718Card.jpg"
-        },
-        {
-            model: "Taycan",
-            price: "$82,700",
-            picture: "/home/tyler/angular_ws/porsche/src/assets/718Card.jpg"
-        }
-    ]
+    {
+        model: "718",
+        price: "$60,500",
+        picture: "/home/tyler/angular_ws/porsche/src/assets/718Card.jpg"
+    },
+    {
+        model: "911",
+        price: "$101,200",
+        picture: "/home/tyler/angular_ws/porsche/src/assets/718Card.jpg"
+    },
+    {
+        model: "Taycan",
+        price: "$82,700",
+        picture: "/home/tyler/angular_ws/porsche/src/assets/718Card.jpg"
+    }
+]
