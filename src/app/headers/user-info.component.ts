c614c923b450bec14c4b7ab05220d2a30152e39b
tsw0015 <tsw0015@mix.wvu.edu>
2021-11-02 16:48:43 -0700

    Revert "Fixed merge stuff"
    This reverts commit bef49fa8dd05296243b5e6d6a30fb0973b7e4a83.

--- src/app/headers/user-info.component.ts
+++ src/app/headers/user-info.component.ts
@@ -1,8 +1,8 @@
-import { Component, Injectable, OnInit } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { UserInfo } from "./user-info.model";
 import { UserInfoService } from "./user-info.service";
 
-@Injectable()
+
 @Component({
     selector: "porsche-user-info",
     templateUrl: './user-info.component.html',
@@ -15,11 +15,11 @@ export class UserInfoComponent implements OnInit{
     }
 
     ngOnInit(): void {
-        console.log("Sending a get request to the server")
-        this.userInfoService.getUserInfo();
         console.log("Registering showUserInfo as a subscriber");
         this.showUserInfo();
     }
+
+    
     showUserInfo() {
         this.userInfoService.getUserInfo().subscribe((data: UserInfo) => {
             console.log(data);
