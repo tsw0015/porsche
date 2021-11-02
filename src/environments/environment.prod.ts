b7f7146ff3c9fe7bc502ca6439173ed3a4aca7dc
Tyler <tyler@pop-os.localdomain>
2021-11-02 17:55:44 -0400

    Built Webapp

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,21 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAuouY53kujjbp3dGn6z8Ah_CHFRf_roic",
+
+  authDomain: "porsche-21768.firebaseapp.com",
+
+  databaseURL: "https://porsche-21768-default-rtdb.firebaseio.com",
+
+  projectId: "porsche-21768",
+
+  storageBucket: "porsche-21768.appspot.com",
+
+  messagingSenderId: "75251784762",
+
+  appId: "1:75251784762:web:6886632cf142d00a812903",
+
+  measurementId: "G-2W9WVE796D"
+
+  }
 };
